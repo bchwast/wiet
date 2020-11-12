708008015d1d1d04b966a6d8b786421a75bdf809
bchwast <bchwast01@gmail.com>
2020-11-12 12:52:19 +0100

    okres

--- WDI/Ćwiczenia 3/ex_19.py
+++ WDI/Ćwiczenia 3/ex_19.py
@@ -1,61 +1,39 @@
-a = int(input("> "))
-b = int(input("> "))
+def ile25(n):
+    licznik2 = 0
+    while n%2 == 0:
+        licznik2 += 1
+        n //= 2
 
-b2 = b
-check = False
-count = 0
-count2 = 0
-count5 = 0
-i = 2
-while i <= b2:
-    while b2 % i == 0:
-        if i == 3 or i > 5:
-            check = True
-        if i == 2:
-            count2 += 1
-        if i == 5:
-            count5 += 1
-        b2 //= i
-    i += 1
+    licznik5 = 0
+    while n%5 == 0:
+        licznik5 += 1
+        n //= 5
 
-if count2 >= count5:
-    count = count2
-else:
-    count = count5
+    return max(licznik2, licznik5)
+#end def
 
-print(a//b, end=".")
-a = a%b
 
-ac = a
-bc = b
-if check == True:
-    while count > 0:
-        ac *= 10
-        if ac > bc:
-            print(ac//bc, end="")
-            ac = ac%bc
-        else:
-            print(0, end="")
-        count -= 1
+licznik = int(input("> "))
+mianownik = int(input("> "))
 
-    a = a//(2**count2)
-    a = a//(5**count5)
-    b = b//(2**count2)
-    b = b//(5**count5)
-
-    x = 9
-    dig9 = 1
-    while x%b != 0:
-        x = x*10 + 9
-        dig9 += 1
-
-    print("(", end="")
-    while dig9 > 0:
-        ac *= 10
-        if ac > bc:
-            print(ac//bc, end="")
-            ac = ac%bc
-        else:
-            print(0, end="")
-        dig9 -= 1
-    print(")")
+print(licznik//mianownik, end="")
+reszta = licznik%mianownik
+if reszta > 0:
+    print(".", end="")
+    for i in range(ile25(mianownik)):
+        reszta *= 10
+        print(reszta//mianownik, end="")
+        reszta %= mianownik
+    #end for
+    if reszta > 0:
+        print("(", end="")
+        mem = reszta
+        while True:
+            reszta *= 10
+            print(reszta//mianownik, end="")
+            reszta %= mianownik
+            if reszta == mem:
+                break
+        #end while
+        print(")")
+#end if
