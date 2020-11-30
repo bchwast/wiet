4a6dbdae763a12de05df8160598466fe3672bcb8
bchwast <bchwast01@gmail.com>
2020-11-30 16:42:07 +0100

    to nie dziala

---
+++ WDI/Ćwiczenia 8_9/ex_10.py
@@ -0,0 +1,12 @@
+def wyznacznik(M, n):
+    if n == 1:
+        return M[0][0]
+    if n == 2:
+        return M[0][0]*M[1][1] - M[0][1]*M[1][0]
+    for i in range(n):
+        matrix = M[]
+        det += ((-1)**(i))*M[]
+#end def
+
+
+M = []
